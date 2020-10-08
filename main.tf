92829f4fcdbf23a1eb173e4b114baec2385a964b
Frederic Descamps <lefred.descamps@gmail.com>
2020-10-08 15:39:28 +0200

    fix a warning message
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- main.tf
+++ main.tf
@@ -175,7 +175,7 @@ resource "oci_core_subnet" "private" {
   compartment_id             = var.compartment_ocid
   vcn_id                     = oci_core_virtual_network.mysqlvcn.id
   route_table_id             = oci_core_route_table.private_route_table.id
-  security_list_ids          = ["${oci_core_security_list.private_security_list.id}"]
+  security_list_ids          = [oci_core_security_list.private_security_list.id]
   dhcp_options_id            = oci_core_virtual_network.mysqlvcn.default_dhcp_options_id
   prohibit_public_ip_on_vnic = "true"
   dns_label                  = "mysqlpriv"
