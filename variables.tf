439cedc6f61df4f2fc6244069c6e076d58865432
Frederic Descamps <lefred.descamps@gmail.com>
2020-10-08 19:59:20 +0200

    change shape in variables
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- variables.tf
+++ variables.tf
@@ -62,7 +62,7 @@ variable "node_image_id" {
 
 variable "node_shape" {
   description = "Instance shape to use for master instance. "
-  default     = "VM.Standard1.1"
+  default     = "VM.Standard.E2.1"
 }
 
 variable "mysql_root_password" {
